a4d9a99d55396ee6dfd19bc46955fcaaaf165245
AllanJohnson01 <ajohnson@bigsquid.com>
2018-01-25 22:47:52 -0700

    setup fetch for sending the data to the server and handle a response

--- server.js
+++ server.js
@@ -1,17 +1,43 @@
 const express = require('express');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = 5000;
 
-var fs = require('fs');
-app.get('/ping', function (req, res) {
-    console.log('Happy Times');
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+  next();
+});
 
-    fs.appendFile('mynewfile1.txt', 'Hello content!', function (err) {
-        if (err) throw err;
-        console.log('Saved!');
-    });
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post('/article', function(req, res) {
+  if (!req.body) return res.sendStatus(400);
+  console.log('Happy Times');
 
-    return res.send('pong');
+  const title = req.body.title;
+  const subtitle = req.body.subtitle;
+  const description = req.body.description;
+  const image = req.body.image;
+
+  fs.appendFile('mynewfile1.txt', 'Hello content!', function(err) {
+    if (err) throw err;
+    console.log('Saved!');
+  });
+
+  return res.send(
+    JSON.stringify({
+      title: title,
+      subtitle: subtitle,
+      description: description,
+      image: image,
+    }),
+  );
 });
 app.listen(port, () => console.log(`Server responded on port ${port}`));
--- src/AddArticleForm.js
+++ src/AddArticleForm.js
@@ -1,54 +1,103 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './index.css';
 
+const sendData = data => {
+  const myHeaders = new Headers({
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+  });
+
+  const headerOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    mode: 'cors',
+    cache: 'default',
+    body: JSON.stringify(data),
+  };
+
+  fetch('http://localhost:5000/article', headerOptions)
+    .then(res => {
+      return res.json();
+    })
+    .then(data => console.log('here is my data: ', data))
+    .catch(e => console.error(e));
+};
+
 class AddArticleForm extends Component {
-    constructor() {
-        super();
-        this.state = {
-            articles: []
-        }
-    }
+  constructor() {
+    super();
+    this.state = {
+      articles: [],
+    };
+  }
 
-    createArticle (event, index) {
-        event.preventDefault();
-        const article = {
-            title: this.title.value,
-            subtitle: this.subtitle.value,
-            description: this.description.value,
-            image: this.image.value,
-        };
-        console.log(article);
-        this.props.addArticle(article, index);
-    }
+  createArticle(event, index) {
+    event.preventDefault();
+    const article = {
+      title: this.title.value,
+      subtitle: this.subtitle.value,
+      description: this.description.value,
+      image: this.image.value,
+    };
 
-    addArticleForm() {
-        const item = this.state.articles;
-        const title = '';
-        const subtitle = '';
-        const description = '';
-        const image = '';
-        item.push({title, subtitle, description, image});
-        this.setState({articles: item});
-    }
+    //this.props.addArticle(article, index);
+    sendData(article);
+  }
 
+  addArticleForm() {
+    const item = this.state.articles;
+    const title = '';
+    const subtitle = '';
+    const description = '';
+    const image = '';
+    item.push({ title, subtitle, description, image });
+    this.setState({ articles: item });
+  }
 
-    render () {
-        return (
-            <div className="grid-container">
-                <button className="button alert" onClick={this.addArticleForm.bind(this)}>Add Article</button>
-                {this.state.articles.map((item, index) => {
-                    return (
-                        <form className="v" key={index} onSubmit={(e) => this.createArticle(e, index)}>
-                            <input ref={(input) => this.title = input} type="text" placeholder="Title"/>
-                            <input ref={(input) => this.subtitle = input} type="text" placeholder="Subtitle"/>
-                            <input ref={(input) => this.description = input} type="text" placeholder="Description"/>
-                            <input ref={(input) => this.image = input} type="text" placeholder="Image"/>
-                            <button className="button" type="submit">Save Form</button>
-                        </form>
-                    )
-                })}
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div className="grid-container">
+        <button
+          className="button alert"
+          onClick={this.addArticleForm.bind(this)}
+        >
+          Add Article
+        </button>
+        {this.state.articles.map((item, index) => {
+          return (
+            <form
+              className="v"
+              key={index}
+              onSubmit={e => this.createArticle(e, index)}
+            >
+              <input
+                ref={input => (this.title = input)}
+                type="text"
+                placeholder="Title"
+              />
+              <input
+                ref={input => (this.subtitle = input)}
+                type="text"
+                placeholder="Subtitle"
+              />
+              <input
+                ref={input => (this.description = input)}
+                type="text"
+                placeholder="Description"
+              />
+              <input
+                ref={input => (this.image = input)}
+                type="text"
+                placeholder="Image"
+              />
+              <button className="button" type="submit">
+                Save Form
+              </button>
+            </form>
+          );
+        })}
+      </div>
+    );
+  }
 }
 export default AddArticleForm;
