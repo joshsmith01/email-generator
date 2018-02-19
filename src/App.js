3b6dad469d8ab5554c2f25d733b11e76dd06a9d1
Joshua Smith <joshsmith01@me.com>
2018-02-19 15:01:52 -0800

    Take another approach to this

--- src/App.js
+++ src/App.js
@@ -2,17 +2,58 @@ import React, { Component } from 'react';
 import logo from './dice_logo.svg';
 import './App.css';
 import Footer from './Footer';
-import AddArticleForm from './AddArticleForm';
+// import AddArticleForm from './AddArticleForm';
+
+// Use this. It's better. -JMS
+// https://stackoverflow.com/questions/42316604/how-to-implement-a-dynamic-form-with-controlled-components-in-react-js
 
 class App extends Component {
-  constructor() {
-    super();
-    this.addArticle = this.addArticle.bind(this);
-    this.state = {
-      articles: {},
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {articles: []};
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
+    createUI() {
+        return this.state.articles.map((el, i) =>
+            <div key={i}>
+                <input ref={(input) => this.title = input} type="text" value={el || ''} onChange={this.handleChange.bind(this, i)} placeholder="Title"/>
+                <input ref={(input) => this.subtitle = input} type="text" value={el || ''} onChange={this.handleChange.bind(this, i)} placeholder="Subtitle"/>
+                <input ref={(input) => this.description = input} type="text" value={el || ''} onChange={this.handleChange.bind(this, i)} placeholder="Description"/>
+                <input ref={(input) => this.image = input} type="text" value={el || ''} onChange={this.handleChange.bind(this, i)} placeholder="Image"/>
+                <input className='button alert' type='button' value='Remove Article' onClick={this.removeClick.bind(this, i)}/>
+            </div>
+        )
+    }
+
+    handleChange(i, event) {
+        let articles = [...this.state.articles];
+        articles[i] = event.target.value;
+        this.setState({articles});
+
+    }
+
+    addClick() {
+        this.setState(prevState => ({articles: [...prevState.articles, '']}))
+    }
+
+    removeClick(i) {
+        let articles = [...this.state.articles];
+        articles.splice(i, 1);
+        this.setState({articles});
+    }
+
+    handleSubmit(event) {
+        console.log(this.state);
+        fetch('http://localhost:5000/article', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(this.state)
+        }).then(console.log('hi from submitFullForm'))
+    }
   addArticle(article, index) {
     // update state
     const articles = { ...this.state.articles };
@@ -45,8 +86,14 @@ class App extends Component {
         </header>
         <section className="small-12 cell grid-padding-x">
           <h2>AddArticleForm</h2>
-          <AddArticleForm addArticle={this.addArticle} />
-            <button className="button" type="submit" onClick={() => this.submitFullForm()}>Submit Entire Form</button>
+            <form onSubmit={this.handleSubmit}>
+                <div className="grid-container">
+                {this.createUI()}
+                <input className='button success' type='button' value='Add Article' onClick={this.addClick.bind(this)}/>
+                <br/>
+                <input className='button' type="submit" value="Submit Form"/>
+                </div>
+            </form>
         </section>
         <Footer />
       </div>
